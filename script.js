1f54ee79fd82e49544d1d684777143fea4cc471a
Andrey Pudov <mail@andreypudov.com>
2023-08-24 02:22:54 +0200

    added case with image files

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function share(url) {
+const getDate = async (pageUrl, imageUrl) => {
+  var files = [];
+
+  if (imageUrl !== undefined) {
+    const blob = await fetch(imageUrl).then(r => r.blob());
+    files = [ new File([blob], 'bear.png', { type: 'image/png' }), ];
+  }
+
   const data = {
-    url: url,
+    files: files,
+    url: pageUrl,
   };
-    
+
+  return data;
+}
+
+const share = async (pageUrl, imageUrl) => {
+  const data = await getDate(pageUrl, imageUrl);
+
   if (navigator.share) {
     navigator.share(data)
       .then(() => console.log('Successful share'))
       .catch((error) => console.log('Error sharing', error));
   }
-} 
+}
